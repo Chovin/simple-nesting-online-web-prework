93a31f10be46bbf2041a68016a2f36437a19195f
Chovin Carlson <Chovin@users.noreply.github.com>
2018-09-11 22:12:59 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -56,7 +56,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-	programmer_hash[:dennis_ritchie][:languages]p0
+	programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
@@ -87,6 +87,7 @@ def adding_matz
 		:known_for => "Ruby",
 		:languages => ["LISP", "C"]
 	}
+	programmer_hash
 end
 
 def changing_alan
